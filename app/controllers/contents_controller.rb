d5e6664921f415ac9607932dd404319ef3c91a27
Guillaume Barillot <gbarillot@gmail.com>
2018-11-18 20:11:40 +0100

    Update contents_controller.rb

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -4,13 +4,15 @@ class ContentsController < ApplicationController
   
   def index
     @content = Content.all
+    # On était sensé se limiter à 5
   end
 
   def create
     Content.create!(
       contenu: params[:comment]
     )
-
+    # Et au cas où la validation ne passe pas, que fait-on ?
+    
     redirect_to contents_path
   end
   # Il manque des features !
