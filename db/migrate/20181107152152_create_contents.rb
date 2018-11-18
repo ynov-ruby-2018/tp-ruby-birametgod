78e17f69d853ff2a77d8f4f1439bc37583caaba6
Guillaume Barillot <gbarillot@gmail.com>
2018-11-18 20:16:58 +0100

    Update 20181107152152_create_contents.rb

--- db/migrate/20181107152152_create_contents.rb
+++ db/migrate/20181107152152_create_contents.rb
@@ -2,7 +2,7 @@ class CreateContents < ActiveRecord::Migration[5.2]
   def change
     create_table :contents do |t|
       t.text :contenu
-
+      # manque t.integer user_id !
       t.timestamps
     end
   end
