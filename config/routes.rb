f8fa684bee07d4a237b883b598354d64ddeeb028
Guillaume Barillot <gbarillot@gmail.com>
2018-11-18 20:17:58 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  mount LikeDislike::Engine, at: '/'
+  mount LikeDislike::Engine, at: '/' # WTF ?! Vous avez vraiment fait une Gem derrière ça ??!
+  
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
